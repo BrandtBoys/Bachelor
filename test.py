cc77d3ab345649e56fd80745da49eb7b94bc8bd2
Markus Brandt Højgaard <mbrh@itu.dk>
2025-03-01 23:00:40 +0100

    change ef name to local PAT

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import uuid
 GITHUB_OWNER = "Brandtboys"  # Change this
 REPO_NAME = "Bachelor"  # Change this
 WORKFLOW_NAME = "update-docs.yml"  # Change if different
-GITHUB_TOKEN = os.getenv("GITHUB_PAT")  # Use a Personal Access Token
+GITHUB_TOKEN = os.getenv("$GITHUB_PAT")  # Use a Personal Access Token
 
 # Commits to compare (replace or allow user input)
 current_commit = "dbf0743"  # Example commit hash or HEAD~1
