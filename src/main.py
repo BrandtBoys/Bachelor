baa26c66f8ae9217ff706bcb8847897f6e710c6e
Lukas <lupa@itu.dk>
2025-03-03 14:44:00 +0100

    Add comments to main ...

--- src/main.py
+++ src/main.py
@@ -3,17 +3,22 @@ def mul(a,b):
 
 def calculate(a):
     res = 1
-    while a > 0:
+    while a > 0: #Test comment
         res = mul(res,a)
         a = a -1
     return res
 
 def sub(a,b):
+    #Comment
+    print("Test print")
     return a-b
 
 def enough_balance_to_do_stuff(balance, amount):
-    return sub(balance, amount) > 0
+    return sub(balance, amount) > 0 #Test comment
 
 def not_enough_balance_to_do_stuff(balance, amount):
     print(amount)
     return not sub(balance, amount) > 0
+
+
+
