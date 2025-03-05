09376768fd4dc7dd7385e49b47f3099ffaf368bf
Lukas <lupa@itu.dk>
2025-03-05 10:58:08 +0100

    ...

--- src/main.py
+++ src/main.py
@@ -9,16 +9,26 @@ def calculate(a):
     return res
 
 def sub(a,b):
-    #Comment
+    #Here we print something
     print("Test print")
     return a-b
 
 def enough_balance_to_do_stuff(balance, amount):
-    return sub(balance, amount) > 0 #Test comment
+    return sub(balance, amount) > 0 #Subtract from balance
 
 def not_enough_balance_to_do_stuff(balance, amount):
     print(amount)
     return not sub(balance, amount) > 0
+
+def buyPizza(price, balance):
+    #Update balance to reflect payment
+    balance = balance - price
+    pizza = {
+        "price": price,
+        "type": "pepperoni",
+        "size": "huge"
+    }
+    return pizza
 
 
 
