c94c13c95e7748da47085d11c6fcfded7799c37e
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-03-07 11:52:09 +0100

    Updated ${file_name} in the test environment

--- src/main.py
+++ src/main.py
@@ -1,47 +1,42 @@
-def mul(a,b):
-    # This function multiplies two numbers and returns the result
-    return a*b
-
-def calculate(a):
-    # Initialize a variable to store the result, starting at 1
-    res = 1
-    
-    # Loop as long as 'a' is greater than 0
-    while a > 0: 
-        # Multiply the current result by 'a'
-        res = mul(res,a)
-        
-        # Subtract 1 from 'a' for each iteration
-        a = a -1
-        
-    # Return the final result
-    return res
-
-def sub(a,b):
-    # This function subtracts two numbers and returns the difference
-    print("Test print")
-    return a-b
-
-def enough_balance_to_do_stuff(balance, amount):
-    # Check if there is enough balance to do something (i.e., 'balance' minus 'amount' is greater than 0)
-    return sub(balance, amount) > 0 
-
-def not_enough_balance_to_do_stuff(balance, amount):
-    print(amount)
-    # Return the opposite of whether there is enough balance
-    return not sub(balance, amount) > 0
-
-# This function simulates buying a pizza by subtracting its price from the balance and returning the pizza details
-def buyPizza(price, balance):
-    # Subtract the pizza price from the balance
-    balance = balance - price
-    
-    # Create a dictionary with the pizza's details
-    pizza = {
-        "price": price,
-        "type": "pepperoni",
-        "size": "huge"
-    }
-    
-    # Return the pizza details
-    return pizza
+  def mul(a,b):
+      return a*b
+  
+  def calculate(a):
+      res = 1
+-     while a > 0: #Test comment
++     while a > 0: 
+          res = mul(res,a)
+          a = a -1
+      return res
+  
+  def sub(a,b):
+-     #Comment
++     
+      print("Test print")
+      return a-b
+  
+  def enough_balance_to_do_stuff(balance, amount):
+-     return sub(balance, amount) > 0 #Test comment
+?                                     -------------
++     return sub(balance, amount) > 0 
+  
+  def not_enough_balance_to_do_stuff(balance, amount):
+      print(amount)
+      return not sub(balance, amount) > 0
+  
++ def buyPizza(price, balance):
++     
++     new_balance = sub(balance, price)
++     pizza = {
++         "price": price,
++         "type": "pepperoni",
++         "size": "huge"
++     }
++     
++     enough = not_enough_balance(new_balance, price)
++     if enough:
++         return pizza
++     else return None
+  
+  
++ 
