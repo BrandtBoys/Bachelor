8f4fec01bf62d46948bb48a56d5dd9d7c54ea483
palleBrandt <lupa@itu.dk>
2025-03-07 15:03:39 +0100

    Add incoming files, replicated commit without comments.

--- src/main.py
+++ src/main.py
@@ -3,32 +3,38 @@ def mul(a,b):
 
 def calculate(a):
     res = 1
-    while a > 0: #Test comment
+    while a > 0: 
         res = mul(res,a)
         a = a -1
     return res
 
 def sub(a,b):
-    #Here we print something
+  #Here we print something
+    
     print("Test print")
     return a-b
 
 def enough_balance_to_do_stuff(balance, amount):
-    return sub(balance, amount) > 0 #Subtract from balance
+    return sub(balance, amount) > 0 
 
 def not_enough_balance_to_do_stuff(balance, amount):
     print(amount)
     return not sub(balance, amount) > 0
 
 def buyPizza(price, balance):
-    #Update balance to reflect payment
-    balance = balance - price
+  #Update balance to reflect payment
+    
+    new_balance = sub(balance, price)
     pizza = {
         "price": price,
         "type": "pepperoni",
         "size": "huge"
     }
-    return pizza
+    
+    enough = not_enough_balance(new_balance, price)
+    if enough:
+        return pizza
+    else return None
 
 
 
