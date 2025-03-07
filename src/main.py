4d3da4fd741ef807fb808b1e2362a7638d47400e
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-03-07 15:04:53 +0100

    Updated ${file_name} in the test environment

--- src/main.py
+++ src/main.py
@@ -1,45 +1,54 @@
 def mul(a,b):
-    # This function multiplies two numbers together using a loop.
-    # It starts with the result being 1, then for each number in the input,
-    # it multiplies the current result by that number and decrements the counter.
+  # This function multiplies two numbers together using a loop.
+  # It starts with the result being 1, then for each number in the input,
+  # it multiplies the current result by that number and decrements the counter.
     return a*b
 
 def calculate(a):
     res = 1
     while a > 0: 
-        # The result is multiplied by 'a' on each iteration of the loop.
+      # The result is multiplied by 'a' on each iteration of the loop.
         res = mul(res,a)
-        # The value of 'a' is decreased by 1 after each multiplication.
+      # The value of 'a' is decreased by 1 after each multiplication.
         a = a -1
     return res
 
 def sub(a,b):
-    # This function subtracts two numbers together and prints a test message.
+  # This function subtracts two numbers together and prints a test message.
+    
     print("Test print")
-    # The result of the subtraction is returned.
+  # The result of the subtraction is returned.
     return a-b
 
 def enough_balance_to_do_stuff(balance, amount):
-    # This function checks if there's enough balance to do something by subtracting
-    # 'amount' from 'balance'. It returns True if the result is positive (i.e., there's
-    # enough balance), and False otherwise.
+  # This function checks if there's enough balance to do something by subtracting
+  # 'amount' from 'balance'. It returns True if the result is positive (i.e., there's
+  # enough balance), and False otherwise.
     return sub(balance, amount) > 0 
 
 def not_enough_balance_to_do_stuff(balance, amount):
-    # This function checks if there's not enough balance to do something by checking
-    # if the subtraction of 'amount' from 'balance' is not positive (i.e., there's
-    # not enough balance). It prints the value of 'amount' and returns True if
-    # there's not enough balance, and False otherwise.
+  # This function checks if there's not enough balance to do something by checking
+  # if the subtraction of 'amount' from 'balance' is not positive (i.e., there's
+  # not enough balance). It prints the value of 'amount' and returns True if
+  # there's not enough balance, and False otherwise.
     print(amount)
     return not sub(balance, amount) > 0
 
 def buyPizza(price, balance):
-    # This function simulates buying a pizza by subtracting the price from the balance,
-    # then returning a dictionary with details about the pizza being bought.
-    balance = balance - price
+    
+  # This function simulates buying a pizza by subtracting the price from the balance,
+  # then returning a dictionary with details about the pizza being bought.
+    new_balance = sub(balance, price)
     pizza = {
         "price": price,
         "type": "pepperoni",
         "size": "huge"
     }
-    return pizza
+    
+    enough = not_enough_balance(new_balance, price)
+    if enough:
+        return pizza
+    else return None
+
+
+
