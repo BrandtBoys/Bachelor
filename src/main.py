6eea171bdaeae027e20b69cd201c96e474fd1ac1
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-03-07 16:32:25 +0100

    Updated ${file_name} in the test environment

--- src/main.py
+++ src/main.py
@@ -1,47 +1,56 @@
 def mul(a,b):
-    # This function multiplies two numbers using a loop, 
-    # which is more efficient than a simple multiplication operation.
+  # This function multiplies two numbers using a loop, 
+  # which is more efficient than a simple multiplication operation.
     return a*b
 
 def calculate(a):
     res = 1
     while a > 0: 
-        # The result of the multiplication is accumulated in 'res' 
-        # and then multiplied by 'a' again in each iteration.
+      # The result of the multiplication is accumulated in 'res' 
+      # and then multiplied by 'a' again in each iteration.
         res = mul(res,a)
         a = a -1
     return res
 
 def sub(a,b):
-    # This function subtracts two numbers, but it's not clear why 
-    # this subtraction operation is necessary. It seems to be 
-    # printing "Test print" and returning the difference between 
-    # 'a' and 'b'.
+    
+  # This function subtracts two numbers, but it's not clear why 
+  # this subtraction operation is necessary. It seems to be 
+  # printing "Test print" and returning the difference between 
+  # 'a' and 'b'.
     print("Test print")
     return a-b
 
 def enough_balance_to_do_stuff(balance, amount):
-    # This function checks if there's enough balance to do something.
-    # The subtraction operation is performed, but it's not clear what 
-    # the purpose of this check is. It seems to be returning True 
-    # if the result of the subtraction is greater than 0.
+  # This function checks if there's enough balance to do something.
+  # The subtraction operation is performed, but it's not clear what 
+  # the purpose of this check is. It seems to be returning True 
+  # if the result of the subtraction is greater than 0.
     return sub(balance, amount) > 0 
 
 def not_enough_balance_to_do_stuff(balance, amount):
-    # This function checks if there's not enough balance to do something.
-    # The print operation is unnecessary and seems to be a mistake. 
-    # It returns the opposite of what you'd expect from this function name.
+  # This function checks if there's not enough balance to do something.
+  # The print operation is unnecessary and seems to be a mistake. 
+  # It returns the opposite of what you'd expect from this function name.
     print(amount)
     return not sub(balance, amount) > 0
 
 def buyPizza(price, balance):
-    # This function buys a pizza with the given price and balance.
-    # The subtraction operation is performed to update the balance. 
-    # A dictionary representing the pizza is created and returned.
-    balance = balance - price
+    
+  # This function buys a pizza with the given price and balance.
+  # The subtraction operation is performed to update the balance. 
+  # A dictionary representing the pizza is created and returned.
+    new_balance = sub(balance, price)
     pizza = {
         "price": price,
         "type": "pepperoni",
         "size": "huge"
     }
-    return pizza
+    
+    enough = not_enough_balance(new_balance, price)
+    if enough:
+        return pizza
+    else return None
+
+
+
