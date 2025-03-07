e7fbe4d75b8b0cacbc9155db7e305b5147205b32
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-03-07 14:58:39 +0100

    Updated ${file_name} in the test environment

--- src/main.py
+++ src/main.py
@@ -1,37 +1,46 @@
 def mul(a,b):
-    # This function multiplies two numbers using a loop, 
-    # which is more efficient than a simple multiplication operation.
+  # This function multiplies two numbers using a loop, 
+  # which is more efficient than a simple multiplication operation.
     return a*b
 
 def calculate(a):
     res = 1
     while a > 0: 
-        # The result of the multiplication is stored in 'res' and then multiplied by 'a' again,
-        # effectively multiplying all numbers from 1 to 'a'.
+      # The result of the multiplication is stored in 'res' and then multiplied by 'a' again,
+      # effectively multiplying all numbers from 1 to 'a'.
         res = mul(res,a)
         a = a -1
     return res
 
 def sub(a,b):
-    # This function subtracts two numbers, but it's not clear why this is necessary.
+  # This function subtracts two numbers, but it's not clear why this is necessary.
+    
     print("Test print")
     return a-b
 
 def enough_balance_to_do_stuff(balance, amount):
-    # The balance should be sufficient to do stuff if the result of the subtraction is greater than 0.
+  # The balance should be sufficient to do stuff if the result of the subtraction is greater than 0.
     return sub(balance, amount) > 0 
 
 def not_enough_balance_to_do_stuff(balance, amount):
     print(amount)
-    # This function returns True if the balance is not enough to do stuff, False otherwise.
+  # This function returns True if the balance is not enough to do stuff, False otherwise.
     return not sub(balance, amount) > 0
 
 def buyPizza(price, balance):
-    # The price of a pizza is subtracted from the balance, and then a dictionary with pizza details is returned.
-    balance = balance - price
+  # The price of a pizza is subtracted from the balance, and then a dictionary with pizza details is returned.
+    
+    new_balance = sub(balance, price)
     pizza = {
         "price": price,
         "type": "pepperoni",
         "size": "huge"
     }
-    return pizza
+    
+    enough = not_enough_balance(new_balance, price)
+    if enough:
+        return pizza
+    else return None
+
+
+
