7e80ff2d3c9471f546f56a814aeda544df52313a
palleBrandt <lupa@itu.dk>
2025-03-07 15:08:55 +0100

    Add incoming files, replicated commit without comments.

--- src/main.py
+++ src/main.py
@@ -1,31 +1,33 @@
 def mul(a,b):
-    # This function multiplies two numbers using a loop to avoid overflow for large inputs.
+  # This function multiplies two numbers using a loop to avoid overflow for large inputs.
     return a*b
 
 def calculate(a):
     res = 1
     while a > 0: 
-        # We use this function to multiply the result by 'a' in each iteration, effectively calculating a^b.
+      # We use this function to multiply the result by 'a' in each iteration, effectively calculating a^b.
         res = mul(res,a)
         a = a -1
     return res
 
 def sub(a,b):
-    # This function subtracts two numbers and prints "Test print" as a test statement.
+  # This function subtracts two numbers and prints "Test print" as a test statement.
+    
     print("Test print")
     return a-b
 
 def enough_balance_to_do_stuff(balance, amount):
-    # We use this function to check if there's enough balance to do something by subtracting 'amount' from 'balance'.
+  # We use this function to check if there's enough balance to do something by subtracting 'amount' from 'balance'.
     return sub(balance, amount) > 0 
 
 def not_enough_balance_to_do_stuff(balance, amount):
     print(amount)
-    # This function checks if there's not enough balance to do something by negating the result of the previous function.
+  # This function checks if there's not enough balance to do something by negating the result of the previous function.
     return not sub(balance, amount) > 0
 
 def buyPizza(price, balance):
-    # We update 'balance' to reflect payment by subtracting 'price'.
+  # We update 'balance' to reflect payment by subtracting 'price'.
+    
     new_balance = sub(balance, price)
     pizza = {
         "price": price,
@@ -33,10 +35,13 @@ def buyPizza(price, balance):
         "size": "huge"
     }
     
-    # This function checks if there's enough balance to buy a pizza and returns the pizza object if possible.
+  # This function checks if there's enough balance to buy a pizza and returns the pizza object if possible.
     enough = not_enough_balance(new_balance, price)
     if enough:
         return pizza
     else:
-        # If there's not enough balance, we return None.
+      # If there's not enough balance, we return None.
         return None
+
+
+
