e5f97c985cd9b00d68f09e696bbbff4f4d1e3c08
palleBrandt <lupa@itu.dk>
2025-03-21 14:16:28 +0100

    Add incoming files, replicated commit without comments.

--- src/new_file_tmp.py
+++ src/new_file_tmp.py
@@ -1,3 +1,5 @@
+
 def test_file():
-    # This function is used to test the basic functionality of printing a message
-    print("Just a test")  # Printing a simple message to verify the file's content
+  # This function is used to test the basic functionality of printing a message
+    
+    print("Just a test")
