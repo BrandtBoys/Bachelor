9c7aa17e21044f46d3fda03d06c9596d889d28db
Markus Brandt Højgaard <118807770+Maquidos@users.noreply.github.com>
2025-04-04 14:17:27 +0200

    Add incoming files, replicated commit without comments.

--- src/main.py
+++ src/main.py
@@ -1,44 +1,54 @@
 def mul(a,b):
-    #Return the multiple of a and b.
+  #Return the multiple of a and b.
+    
     return a*b
 
-#Calculate the factorial of a
+
 def calculate(a):
     res = 1
-    #Multiple a with a-1 recursively as long as a is greater than 0.
+  #Multiple a with a-1 recursively as long as a is greater than 0.
+    
     while a > 0:
         res = mul(res,a)
         a = a -1
     return res
 
 def sub(a,b):
-    #This print is just a test print
+  #This print is just a test print
+    
     print("Test print")
     return a-b
 
-#Checks if there is enough balance to do somehting
+
 def enough_balance_to_do_stuff(balance, amount):
     return sub(balance, amount) > 0
 
-#hello
+
 def not_enough_balance_to_do_stuff(balance, amount):
-    print(amount) #Hello
+    print(amount) 
     return not sub(balance, amount) > 0
 
 def buyPizza(price, balance):
-    #Update balance to reflect payment
+  #Update balance to reflect payment
+    
     new_balance = sub(balance, price)
     pizza = {
         "price": price,
         "type": "pepperoni",
         "size": "huge"
     }
-    #Check if balance is enough to buy pizza
+  #Check if balance is enough to buy pizza
+    
     enough = not_enough_balance_to_do_stuff(new_balance, price)
     if enough:
         return pizza
     else:
         return None
+    
+
+def printIdentity(user):
+    print(f"I am: {user.name}")
+
 
 
 
