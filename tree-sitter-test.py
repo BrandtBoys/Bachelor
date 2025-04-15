8d8abf8c21cf6961a915ee0999abbaa3052ffda5
Lukas <lupa@itu.dk>
2025-04-15 09:32:06 +0200

    Test
    Co-authored-by: Markus Brandt Højgaard <Maquidos@users.noreply.github.com>

---
+++ tree-sitter-test.py
@@ -0,0 +1,12 @@
+from tree_sitter import Parser
+from tree_sitter_languages import get_language
+from dt_diff_lib import tree_sitter_parser_init
+
+
+
+
+comment_py = """
+This is some random commmentt og :attr bla bla
+Este application de computadora 
+Es un commento muy facil!
+"""
