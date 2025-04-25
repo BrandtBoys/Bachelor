865ee51cf2c045ffe69e91638e66325179d8249d
Markus Brandt Højgaard <mbrh@itu.dk>
2025-04-25 13:08:48 +0200

    test setup
    Co-authored-by: Lukas Brandt Pallesen <lupa@itu.dk>

--- agent.py
+++ agent.py
@@ -10,53 +10,77 @@ from tree_sitter_languages import get_language
 import detect_language
 from dt_diff_lib import extract_data, collect_code_comment_range, tree_sitter_parser_init
 
+def main():
 
-repo = git.Repo(".")
+    repo = git.Repo(".")
 
-#to accommodate test-environment: the test script manages branch name and creation
-branch_id = uuid4()
-branch_name = "Update-docs-" + str(branch_id)
-repo.git.branch(branch_name)
-repo.git.checkout(branch_name)
-
-
-# Set the branch name in the GitHub Actions environment
-with open(os.getenv('GITHUB_ENV'), "a") as env_file:
-    env_file.write(f"BRANCH_NAME={branch_name}\n")
-
-# Compare changes and find changed files
-hcommit = repo.head.commit
-
-diff_files = list(hcommit.diff("HEAD~1"))
-
-for file in diff_files:
-    source_path = str(file.a_path)
-    print("Generating comments for diffs in" + source_path)
-    file_language = detect_language.detect_language(source_path)
-    if not file_language:
-            continue
-    h1_content = ""
-    try:
-        h1_commit = repo.commit("HEAD~1")
-        h1_blob = h1_commit.tree / source_path
-        h1_content = h1_blob.data_stream.read().decode("utf-8")
-    except Exception as e:
-        print(e)
-        print("new file incoming!!")
+    #to accommodate test-environment: the test script manages branch name and creation
+    branch_id = uuid4()
+    branch_name = "Update-docs-" + str(branch_id)
+    repo.git.branch(branch_name)
+    repo.git.checkout(branch_name)
 
 
-    with open(source_path, "r") as f:
-        source_code = f.read()
+    # Set the branch name in the GitHub Actions environment
+    with open(os.getenv('GITHUB_ENV'), "a") as env_file:
+        env_file.write(f"BRANCH_NAME={branch_name}\n")
 
-    # Extract all function which is in the diff
-    code_location = extract_data(True, file_language, h1_content, source_code, collect_code_comment_range)
+    # Compare changes and find changed files
+    hcommit = repo.head.commit
 
-    comment_location =[]
-    for code, old_comment, start_byte, end_byte in code_location:
+    diff_files = list(hcommit.diff("HEAD~1"))
 
-        print(old_comment)
+    for file in diff_files:
+        source_path = str(file.a_path)
+        print("Generating comments for diffs in" + source_path)
+        file_language = detect_language.detect_language(source_path)
+        if not file_language:
+                continue
+        h1_content = ""
+        try:
+            h1_commit = repo.commit("HEAD~1")
+            h1_blob = h1_commit.tree / source_path
+            h1_content = h1_blob.data_stream.read().decode("utf-8")
+        except Exception as e:
+            print(e)
+            print("new file incoming!!")
 
-        # Create prompt for LLM
+
+        with open(source_path, "r") as f:
+            source_code = f.read()
+
+        comment_location = run_llm(file_language, h1_content, source_code)
+
+        commented_code = bytearray(source_code.encode("utf-8"))
+        for comment, start_byte, end_byte in reversed(comment_location):
+            commented_code[start_byte:end_byte] = (comment+"\n").encode()
+
+        # Write changes to docs
+        with open(source_path, "w") as f:
+            f.write(commented_code.decode("utf-8"))
+
+        # Add changes
+        add_files = [source_path]
+        repo.index.add(add_files)
+
+    # Commit changes
+    repo.index.commit("Updated inline documentation")
+
+    repo.remotes.origin.push(refspec=f"{branch_name}:{branch_name}",set_upstream=True)
+
+    repo.__del__()
+    exit(0)
+
+if __name__ == "__main__":
+    main()
+
+def validate_response_as_comment(language, response):
+    root = tree_sitter_parser_init(language, response.encode("utf-8"))
+    child = root.children[0]
+    return child.type in ["comment", "block_comment"] or (child.type == "expression_statement" and child.children[0].type == "string" and ((child.children[0].text.startswith(b'"""') and child.children[0].text.endswith(b'"""')) or (child.children[0].text.startswith(b"'''") and child.children[0].text.endswith(b"'''"))))
+
+def generate_llm_response(file_language, code, old_comment):
+     # Create prompt for LLM
         prompt = ChatPromptTemplate.from_template(
             """
             You are a documentation assistant.
@@ -87,37 +111,22 @@ for file in diff_files:
             old_comment = old_comment
         )
 
-        start = time.time()
         # the LLM does it work
         llm = ChatOllama(model="llama3.2", temperature=0.1)
         llm_response = llm.invoke(prompt_input)
-        end = time.time()
-        # print(f"LLM took {end - start:.4f} seconds")
-        # print(llm_response.content)
-        comment_location.append(((llm_response.content), start_byte, end_byte))
+        return llm_response
+    
+    
 
-    commented_code = bytearray(source_code.encode("utf-8"))
-    for comment, start_byte, end_byte in reversed(comment_location):
-        commented_code[start_byte:end_byte] = (comment+"\n").encode()
+def run_llm(file_language, h1_content, source_code):
+    # Extract all function which is in the diff
+    code_location = extract_data(True, file_language, h1_content, source_code, collect_code_comment_range)
 
-    # Write changes to docs
-    with open(source_path, "w") as f:
-        f.write(commented_code.decode("utf-8"))
+    comment_location =[]
+    for code, old_comment, start_byte, end_byte in code_location:
 
-    # Add changes
-    add_files = [source_path]
-    repo.index.add(add_files)
-
-# Commit changes
-repo.index.commit("Updated inline documentation")
-
-# Push changes
-# try:
-#     repo.create_remote("origin", url="git@github.com:BrandtBoyz/Bachelor")
-# except git.exc.GitCommandError as e:
-#     print(f"error: {e}")
-
-repo.remotes.origin.push(refspec=f"{branch_name}:{branch_name}",set_upstream=True)
-
-repo.__del__()
-exit(0)
+        llm_response = generate_llm_response(file_language, code, old_comment)
+        if validate_response_as_comment(file_language, llm_response.content):
+            comment_location.append(((llm_response.content), start_byte, end_byte))
+    
+    return comment_location
